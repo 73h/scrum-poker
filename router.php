aa11c764bd39e93bb906193a0bf3c52c26635456
h73 <heikoschmidt82@gmail.com>
2021-09-04 23:05:29 +0200

    fix problem in .htaccess

--- router.php
+++ router.php
@@ -2,8 +2,8 @@
 
 $uri = $_SERVER["REQUEST_URI"];
 
-if (preg_match('/^\/?api\/?.*$/', $uri)) {
-    $_GET['api'] = preg_replace('/^\/?api\/?(.*)$/', '$1', $uri);
+if (preg_match('/^\/?api\/.*$/', $uri)) {
+    $_GET['api'] = preg_replace('/^\/?api\/(.*)$/', '$1', $uri);
     include 'index.php';
 } else {
     return false;
