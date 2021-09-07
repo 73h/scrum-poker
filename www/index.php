3b9a9bcaa3853c8e19c1813758925cc7fcfc1071
h73 <heikoschmidt82@gmail.com>
2021-09-08 00:49:06 +0200

    error log test

--- www/index.php
+++ www/index.php
@@ -1,5 +1,11 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('ignore_repeated_errors', TRUE);
+ini_set('display_errors', FALSE);
+ini_set('log_errors', TRUE);
+ini_set('error_log', '../../../../logs/error.log');
+
 use src\app\App;
 
 session_start();
