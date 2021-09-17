f96d6f45dfdfb5259d29f3b2613bc33e4964de61
h73 <heikoschmidt82@gmail.com>
2021-09-17 20:49:20 +0200

    workarround for getallheaders implemented

--- src/static/helper.php
+++ src/static/helper.php
@@ -12,3 +12,16 @@ function getRandomString($length): string
     }
     return $id;
 }
+
+if (!function_exists('getallheaders')) {
+    function getallheaders(): array
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (str_starts_with($name, 'HTTP_')) {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
