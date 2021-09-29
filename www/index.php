ee7215d15a9c67ce9eb80a3c60a04fbb465214c7
Heiko <heiko.schmidt@bkk-vbu.de>
2021-09-30 00:45:07 +0200

    : clean slack test

--- www/index.php
+++ www/index.php
@@ -2,9 +2,6 @@
 
 use src\app\App;
 
-file_put_contents('../uri.txt', $_SERVER["REQUEST_URI"]);
-file_put_contents('../post.txt', json_encode($_POST));
-
 require '../src/static/helpers.php';
 require '../autoload.php';
 
