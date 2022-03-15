87928c994817ad581998be85af9967f734ebe4be
h73 <heikoschmidt82@gmail.com>
2022-03-15 20:36:11 +0100

    version increased

--- src/views/View.php
+++ src/views/View.php
@@ -26,7 +26,7 @@ class View
 
     private function getAssetsVersion(): string
     {
-        return '?v2';
+        return '?v3';
     }
 
     private function getJavaScriptTag(string $file): string
