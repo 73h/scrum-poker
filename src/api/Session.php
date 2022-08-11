02839646206590e07583895b01f0cd960b6cbe48
h73 <heikoschmidt82@gmail.com>
2022-08-11 22:20:27 +0200

    add new robo icons

--- src/api/Session.php
+++ src/api/Session.php
@@ -68,7 +68,7 @@ class Session
             $this->users->{$user_id} = (object)[
                 'name' => $name,
                 'token' => getRandomString(16),
-                'icon' => random_int(0, 22),
+                'icon' => random_int(0, 32),
                 'password' => null
             ];
             if ($this->owner === null) {
